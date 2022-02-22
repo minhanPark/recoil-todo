018593dfd9a611b00acf45ca7a7f4eda29440112
박민한 <alsgks1218@naver.com>
2022-02-22 13:22:47 +0900

    base setting

---
+++ src/TodoLists.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+const TodoLists = () => {
+  const { register } = useForm();
+
+  return (
+    <div>
+      <form>
+        <input {...register("email")} placeholder="Email" />
+        <input {...register("firstName")} placeholder="First Name" />
+        <input {...register("lastName")} placeholder="Last Name" />
+        <input {...register("username")} placeholder="Username" />
+        <input {...register("password")} placeholder="Password" />
+        <input {...register("password1")} placeholder="Password1" />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default TodoLists;
